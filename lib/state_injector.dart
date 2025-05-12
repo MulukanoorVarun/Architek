79a8a259ff1f68c9a1eb5e5b970e7819bc6465de
Dell <saicharan.teamup@gmail.com>
2025-05-12 18:24:57 +0530

    committed by charan 12/05/2025 At 6:23 PM

--- lib/state_injector.dart
+++ lib/state_injector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tripfin/Block/Logic/GetPreviousTripHistory/GetPreviousTripHistoryCubit.dart';
+import 'package:tripfin/Block/Logic/GetPreviousTripHistory/GetPreviousTripHistoryRepository.dart';
 import 'package:tripfin/Block/Logic/GetTrip/GetTripCubit.dart';
 import 'package:tripfin/Block/Logic/GetTrip/GetTripRepository.dart';
 import 'package:tripfin/Block/Logic/LogInBloc/login_cubit.dart';
@@ -13,14 +15,26 @@ class StateInjector {
       create: (context) => RemoteDataSourceImpl(),
     ),
     RepositoryProvider<RegisterRepository>(
-      create: (context) => RegisterImpl(remoteDataSource: context.read<RemoteDataSource>()),
+      create:
+          (context) =>
+              RegisterImpl(remoteDataSource: context.read<RemoteDataSource>()),
     ),
 
     RepositoryProvider<LoginRepository>(
-      create: (context) => LoginImpl(remoteDataSource: context.read<RemoteDataSource>()),
+      create:
+          (context) =>
+              LoginImpl(remoteDataSource: context.read<RemoteDataSource>()),
     ),
     RepositoryProvider<GetTripRep>(
-      create: (context) => GetTripImpl(remoteDataSource: context.read<RemoteDataSource>()),
+      create:
+          (context) =>
+              GetTripImpl(remoteDataSource: context.read<RemoteDataSource>()),
+    ),
+    RepositoryProvider<GetPreviousTripRepo>(
+      create:
+          (context) => GetPreviousTripImpl(
+            remoteDataSource: context.read<RemoteDataSource>(),
+          ),
     ),
   ];
 
@@ -30,8 +44,12 @@ class StateInjector {
     ),
     BlocProvider<LoginCubit>(
       create: (context) => LoginCubit(context.read<LoginRepository>()),
-    )  ,  BlocProvider<GetTripCubit>(
+    ),
+    BlocProvider<GetTripCubit>(
       create: (context) => GetTripCubit(context.read<GetTripRep>()),
+    ),
+    BlocProvider<GetPreviousTripHistoryCubit>(
+      create: (context) => GetPreviousTripHistoryCubit(context.read<GetPreviousTripRepo>()),
     ),
   ];
 }
