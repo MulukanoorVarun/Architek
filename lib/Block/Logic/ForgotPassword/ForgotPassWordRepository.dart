e450cea5281816b9639ee2c155a8e5ef87c439af
Ozrit <mulukanoorvarun2001@gmail.com>
2025-05-27 18:08:41 +0530

    committed by vishwa 27/05/2025  at 6:08 PM

---
+++ lib/Block/Logic/ForgotPassword/ForgotPassWordRepository.dart
@@ -0,0 +1,22 @@
+import '../../../Model/SuccessModel.dart';
+import '../../../Services/remote_data_source.dart';
+
+abstract class ForgotPasswordRepository {
+  Future<SuccessModel?> forgotPassword(Map<String, dynamic> data);
+  Future<SuccessModel?> VerifyOtp(Map<String, dynamic> data);
+}
+
+class ForgotPasswordImpl implements ForgotPasswordRepository {
+  final RemoteDataSource remoteDataSource;
+
+  ForgotPasswordImpl({required this.remoteDataSource});
+
+  @override
+  Future<SuccessModel?> forgotPassword(Map<String, dynamic> data) async {
+    return await remoteDataSource.ForgotpasswordApi(data);
+  }
+  @override
+  Future<SuccessModel?> VerifyOtp(Map<String, dynamic> data) async {
+    return await remoteDataSource.VerifyOtp(data);
+  }
+}
